c0f9a893dfee93e8fcf1cd0426f6053aa4a94de8
akadam2 <jaikadam412@gmail.com>
2022-05-01 22:01:04 +0530

    added dynamic data template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post,Sample
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    posts = Post.objects.filter(title__contains="sample").order_by('create_date')
+    return render(request, 'blog/index.html', {'posts':posts})
