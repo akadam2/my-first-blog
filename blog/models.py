927d72898112d7bc23531f6371d6b52175b8698c
akadam2 <jaikadam412@gmail.com>
2022-05-01 17:27:21 +0530

    edited settings.py demo.blog

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Sample(models.Model):
-    name = models.CharField(max_length=25)
-    place = models.CharField(max_length=25)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -24,4 +16,12 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title    
+        return self.title
+        
+class Sample(models.Model):
+    name = models.CharField(max_length=25)
+    place = models.CharField(max_length=25)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name        
