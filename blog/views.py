b488ccea94049abba715a8320bee6fc4fe059117
akadam2 <jaikadam412@gmail.com>
2022-05-02 19:26:26 +0530

    added new page called post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post,Sample
 
@@ -7,3 +7,7 @@ from .models import Post,Sample
 def index(request):
     posts = Post.objects.filter(title__contains="sample").order_by('create_date')
     return render(request, 'blog/index.html', {'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
